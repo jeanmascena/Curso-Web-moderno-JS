ac310dc1ff1b9ccc29c6ee0458ce3759659404a7
jeanmascena <jeanlucasmascena@gmail.com>
2022-09-08 17:36:43 -0300

    web moderno JS

---
+++ Fundamentos, início/Comentario.js
@@ -0,0 +1,12 @@
+// console.log é usado para exibir algo na tela do computador!
+console.log("Passo #3");
+console.log("Passo #1"); // mais um exemplo de console.log!
+
+
+/*
+ * Esse é um 
+ * comentário
+ * de múltiplas
+ * linhas
+ */
+console.log("PAsso #2");
---
+++ Fundamentos, início/Dados.js
@@ -0,0 +1,16 @@
+var nome = "Caneta";
+var quantidade = 10; // valor literal
+let preco = 6.4;
+var imposto = 1.5;
+let precoFinal = preco + imposto;
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(preco + imposto);
+
+nome = "Caneta Bic "
+console.log(nome);
+
+
